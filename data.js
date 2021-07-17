d186d046d940b8036b9672874cf1811560a6b211
eun-chae-s <s9710173@naver.com>
2021-07-18 00:33:13 +0900

    add node js

---
+++ data.js
@@ -0,0 +1,8 @@
+// JSON data into the object
+let json = require('/Users/macbookair/Desktop/Coding Prep/Projects/Course scheduler/data/sample/CSC_courses.json');
+console.log(json, 'the json obj');
+
+// Functions
+function findSection() {
+    var course_code = document.getElementById('course');
+}
--- js/data.js
+++
@@ -1,7 +0,0 @@
-// JSON data into the object
-
-
-// Functions
-function findSection() {
-    var course_code = document.getElementById('course');
-}
