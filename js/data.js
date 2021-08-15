a4592006bb1b1cd0c98265f05e2e46cc1ac63a45
eun-chae-s <s9710173@naver.com>
2021-08-15 14:29:56 +0900

    Coloring the table based on the selected schedule - demo

--- js/data.js
+++ js/data.js
@@ -1,5 +1,12 @@
 // JSON data into the object
 let json_file = [];
+var schedule_days = {
+  "Monday": 1,
+  "Tuesday": 2,
+  "Wednesday": 3,
+  "Thursday": 4, 
+  "Friday": 5
+};
 
 // Clean JSON file
 $.getJSON("data/csc_courses.json", function(json) {
@@ -122,7 +129,9 @@ function findSection() {
 
         // add button
         var button = document.createElement('button');
+        button.id = "add";
         button.innerHTML = "add";
+        
 
         // add all the components to the entire card
         card.appendChild(name);
@@ -133,6 +142,8 @@ function findSection() {
 
         // add the card to the section display
         sections.appendChild(card);
+        
+        document.getElementById("add").addEventListener("click", addCourse(lec, section.section));
       });
 
       section.tutorials.forEach(tut => {
@@ -157,16 +168,24 @@ function findSection() {
         var schedule = document.createElement('a');
         let text = '';
 
-        tut.schedule.forEach(time => {
-          text += time.Day + ": " + time.Start + " - " + time.End;
-        })
+        for (let i = 0; i < tut.schedule.length; i++) {
+          var time = tut.schedule[i];
+          if (i < tut.schedule.length - 1) {
+            text += time.Day + ": " + time.Start + " - " + time.End + "<br>";
+          } else {
+            text += time.Day + ": " + time.Start + " - " + time.End;
+          } 
+            
+        }
         
         schedule.innerHTML = text;
         schedule.style.fontSize = "12px";
 
         // add button
         var button = document.createElement('button');
+        button.id = "add";
         button.innerHTML = "add";
+        
 
         // add all the components to the entire card
         card.appendChild(name);
@@ -177,8 +196,62 @@ function findSection() {
 
         // add the card to the section display
         sections.appendChild(card);
+
+        document.getElementById("add").addEventListener("click", addCourse(tut, section.section));
       })
       
     })
 }
 
+
+// find the timeslot and color the cell on the table
+function addCourse(section, semester) {
+  console.log("success");
+  // course_code
+  var course = document.getElementById('course');
+  var course_code = course.value.toUpperCase();
+
+  // selected section info
+  // 1. section name
+  var sec_name = section.sec_name;
+
+  // 2. section semester
+  var sem = (semester == "F") ? "fall" : (semester == "S") ? "winter" : "both";
+
+  // 3. section schedule array
+  var schedule = section.schedule;
+  schedule.forEach(time => {
+    var day = time.Day;
+    var start = time.Start;
+    var end = time.End;
+    var start_time = parseInt(start.slice(0, start.indexOf(":")));
+    var end_time = parseInt(end.slice(0, end.indexOf(":")));
+    for (let i = 0; i < end_time - start_time; i++) {
+      if (sem != "both") {
+        var t = document.getElementById(sem);
+        var cell = t.rows[start_time - 7 + i].cells[schedule_days[day]];
+        // if (cell.bgColor != "") {
+        //   alert("Conflicted schedule! Choose another section!");
+        // } else {
+          cell.bgColor = "pink";
+          if (end_time - start_time > 1) {
+            if (i == 0) {
+              cell.innerText = course_code;
+            } 
+            if (i == 1) {
+              cell.innerText = sec_name;
+            }
+          } else {
+            cell.innerHTML = course_code + "<br>" + sec_name;
+          }
+        // }
+      }
+
+      // if filled in multiple borders: erase the bottom border line & top border line
+    }
+  })
+
+  
+}
+
+document.getElementById("fall").rows[1].cells[1]
