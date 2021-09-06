377e897059954e6f2f219b8e0c55bee3db882b97
eun-chae-s <s9710173@naver.com>
2021-09-06 08:33:50 -0400

    Saving HTML File into PDF (demo)

--- data.js
+++ data.js
@@ -402,34 +402,28 @@ function findSection() {
 // Save HTML file into PDF
 // reference: https://www.freakyjolly.com/multipage-canvas-pdf-using-jspdf/
 function getPDF(){
-
-  var HTML_Width = $("#schedule").width();
-  var HTML_Height = $("#schedule").height();
-  var top_left_margin = 15;
-  var PDF_Width = HTML_Width+(top_left_margin*2);
-  var PDF_Height = (PDF_Width*1.5)+(top_left_margin*2);
-  var canvas_image_width = HTML_Width;
-  var canvas_image_height = HTML_Height;
-  
-  var totalPDFPages = Math.ceil(HTML_Height/PDF_Height)-1;
-  
-
-  html2canvas($("#schedule")[0],{allowTaint:true}).then(function(canvas) {
-    canvas.getContext('2d');
-    
-    console.log(canvas.height+"  "+canvas.width);
-    
-    
-    var imgData = canvas.toDataURL("image/jpeg", 1.0);
-    var pdf = new jsPDF('p', 'pt',  [PDF_Width, PDF_Height]);
-    pdf.addImage(imgData, 'JPG', top_left_margin, top_left_margin,canvas_image_width,canvas_image_height);
-    
-    
-    for (var i = 1; i <= totalPDFPages; i++) { 
-      pdf.addPage(PDF_Width, PDF_Height);
-      pdf.addImage(imgData, 'JPG', top_left_margin, -(PDF_Height*i)+(top_left_margin*4),canvas_image_width,canvas_image_height);
+  var doc = new jsPDF();
+  var fallHTML = $('.fall-schedule').html();
+  var specialElementHandlers = {
+    '#elementH': function (element, renderer) {
+      return true;
     }
-    
-    pdf.save("HTML-Document.pdf");
-    });
+  };
+
+  doc.fromHTML(fallHTML, 15, 15, {
+    'width': 500,
+    'height': 600,
+    'elementHandlers': specialElementHandlers
+  });
+
+  doc.addPage();
+  var winterHTML = $('.winter-schedule').html();
+  doc.fromHTML(winterHTML, 15, 15, {
+    'width': 500,
+    'height': 600,
+    'elementHandlers': specialElementHandlers
+  })
+
+  doc.save('sampel-document.pdf');
+
 };
